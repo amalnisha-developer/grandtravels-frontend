0b37090dca10afdd9ecb38fec6d85b6974612c38
amalnisha-developer <amalnisha7705@gmail.com>
2025-06-03 02:40:52 -1000

    first commit

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -15,16 +15,14 @@ const ContactForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         try {
-            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/send-email`, {
+            const response = await fetch('http://localhost:5000/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(formData)
             });
-
             if (response.ok) {
                 alert('Message sent successfully!');
                 setFormData({ name: '', email: '', phone: '', message: '' });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <HelmetProvider>
-    <App />
+      <App />
     </HelmetProvider>
-   
-    
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
