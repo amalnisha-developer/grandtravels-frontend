97fc0169f875550ed13f6306a3e76375b8204310
amalnisha-developer <amalnisha7705@gmail.com>
2025-06-18 01:09:55 -1000

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import './Herostyles.css'; 
+import './Herostyles.css';
+import gttImg from "../assets/gtt.png" 
 
 const Hero = (props) => {
   const { cName, heroImg, title, text, buttons, btnClass } = props; // Destructure props
@@ -9,6 +10,10 @@ const Hero = (props) => {
 
     <div className={cName}>
       <img src={heroImg} alt="Hero" />
+    {/* Overlay gttImg */}
+  <div className="gtt-img-container">
+    <img src={gttImg} alt="GTT Logo" className="gtt-img" />
+  </div>
       <div className="hero-text">
         <h1>{title}</h1>
         <p>{text}</p>
